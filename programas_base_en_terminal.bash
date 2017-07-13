4d533b8e87268ba5d2cb45f2ba02e7e8a16aca04
Jhonbeltran <jbeltranleon@gmail.com>
2017-07-12 23:08:32 -0500

    Agrego mas ejemplos de comandos

--- programas_base_en_terminal.bash
+++ programas_base_en_terminal.bash
@@ -9,8 +9,18 @@ Nos permite crear una cadena de la huella digital de un archivo, usando el algor
 ➜ open [archivo] : abre los archivos con el programa que al sistema mejor le parece
 ➜ open -a vim [archivo] : abre el archivo con vim
 
-➜ more index.scss : muestra el archivo y con enter va avanzando y retrocede con b
-➜ less index.scss : nos muestra el archivo en una pantalla que ce cierra con q
+➜ more [archivo] : muestra el archivo y con enter va avanzando y retrocede con b
+➜ less [archivo] : nos muestra el archivo en una pantalla que ce cierra con q
+➜ cat [archivo] : imprime el archivo completo
+
+➜ tail [archivo] : nos muestra las ultimas 10 lineas del archivo
+➜ tail -20 index.scss : ultimas 20 lineas de un archivo
+➜ tail -f [archivo] : espera las modificaciones del archivo y las muestra 
+
+➜ head [archivo] : Te muestra las 10 primeras lineas de un archivo 
+➜ head -12 [archivo] : Te muestra las 12 primeras lineas de un archivo
+
+➜ wc [archivo] : (-l -w -p) muestra lineas palabras caracteres
 
 Para agregar un usuario a un grupo ejecutamos el siguiente comando:
 ➜ sudo addgroup usuario grupo
