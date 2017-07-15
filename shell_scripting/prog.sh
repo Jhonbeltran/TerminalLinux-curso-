23f625541567605e7e73240e73ee168ba33134bb
Jhonbeltran <jbeltranleon@gmail.com>
2017-07-15 16:15:45 -0500

    Agrego definicion de ejecutor

--- shell_scripting/prog.sh
+++ shell_scripting/prog.sh
@@ -1,6 +1,7 @@
 # para que se ejecute el siguiente tiene que haber acabado el primero
 # se ejecuta usando ./prog.sh
 #!/bin/bash
+#El ejecutor es el que nos dice con que se va a ejecutar el archivo
 date
 pwd
 echo "Fin"
